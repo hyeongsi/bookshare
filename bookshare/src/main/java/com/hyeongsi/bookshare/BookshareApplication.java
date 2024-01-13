4b0ce10b85c5395eccb363b7b49b98372ae9bd37
ParkSiHyeong <parksh4603@naver.com>
2024-01-14 00:26:37 +0900

    create project

---
+++ bookshare/src/main/java/com/hyeongsi/bookshare/BookshareApplication.java
@@ -0,0 +1,13 @@
+package com.hyeongsi.bookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshareApplication.class, args);
+	}
+
+}
---
+++ bookshare/src/test/java/com/hyeongsi/bookshare/BookshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeongsi.bookshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
